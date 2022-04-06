f0fc3ccc218547533b189f1b7f226c3cd562fa56
strcurseten <fernandez.kirstenpearl@auf.edu.ph>
2022-04-06 20:26:56 +0800

    Initial commit

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -54,23 +54,23 @@ class StudentsController extends Controller
             'student_3' => $student_3,
             'student_4' => $student_4,
             'student_5' => $student_5,
-            // student 1 grades
+            // Student 1 grades
             's1_midterm' => $request->s1_midterm,
             's1_final' => $request->s1_final,
             's1_average' => $s1_average,
-            // student 2 grades
+            // Student 2 grades
             's2_midterm' => $request->s2_midterm,
             's2_final' => $request->s2_final,
             's2_average' => $s2_average,
-            // student 3 grades
+            // Student 3 grades
             's3_midterm' => $request->s3_midterm,
             's3_final' => $request->s3_final,
             's3_average' => $s3_average,
-            // student 4 grades
+            // Student 4 grades
             's4_midterm' => $request->s4_midterm,
             's4_final' => $request->s4_final,
             's4_average' => $s4_average,
-            // student 5 grades
+            // Student 5 grades
             's5_midterm' => $request->p5_midterm,
             's5_final' => $request->s5_final,
             's5_average' => $s5_average,
